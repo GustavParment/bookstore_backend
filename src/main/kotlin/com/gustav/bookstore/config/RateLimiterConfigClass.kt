2124a65c0c9aa37bb75541f95045df84b0ec1074
GustavParment <gustav.parment1993@gmail.com>
2024-10-14 15:59:04 +0200

    add rate limiter bean

---
+++ src/main/kotlin/com/gustav/bookstore/config/RateLimiterConfigClass.kt
@@ -0,0 +1,22 @@
+package com.gustav.bookstore.config
+
+import io.github.resilience4j.ratelimiter.RateLimiter
+import io.github.resilience4j.ratelimiter.RateLimiterConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+@Configuration
+class RateLimiterConfigClass {
+
+    @Bean
+    fun rateLimiterConfig(): RateLimiter {
+        val config = RateLimiterConfig.custom()
+            .limitForPeriod(10)
+            .limitRefreshPeriod(Duration.ofMinutes(1))
+            .timeoutDuration(Duration.ofSeconds(1))
+            .build()
+        return RateLimiter.of("rateLimiter", config)
+    }
+
+}
